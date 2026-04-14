39f863d8c80a651b477bee5d300588b862f5e9b5
MetaMaaz <40913192+MetaMaaz@users.noreply.github.com>
2026-04-14 14:18:40 +0100

    accept inputs for how many letters and symbols

--- PythonProjects/PasswordGenerator.py
+++ PythonProjects/PasswordGenerator.py
@@ -1,1 +1,4 @@
 print  ("Welcome to the Password Generator!")
+print  ("How many letters would you like in your password?")
+nr_letters = int(input())
+print  ("How many symbols would you like?")                         
